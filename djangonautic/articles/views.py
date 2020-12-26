71ec391d6425b4d59bb2198bf1420de761bc0f06
Harika <h.duyu@rebuy.com>
2020-12-26 12:25:45 +0100

    requiring login

--- djangonautic/articles/views.py
+++ djangonautic/articles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 from .models import Article
 
@@ -11,3 +12,8 @@ def article_list(request):
 def article_detail(request, slug):
     article = Article.objects.get(slug=slug)
     return render(request, 'articles/article_detail.html', {'article': article})
+
+
+@login_required(login_url="/accounts/login/")
+def article_create(request):
+    return render(request, 'articles/article_create.html')
