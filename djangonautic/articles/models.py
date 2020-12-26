3d88b8b321f3bc989151534c31c2a67ad41a0d2c
Harika <h.duyu@rebuy.com>
2020-12-26 14:14:07 +0100

    avoiding pylint warning

--- djangonautic/articles/models.py
+++ djangonautic/articles/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import User
+from django.conf import settings
 from django.db import models
 
 
@@ -8,7 +8,8 @@ class Article(models.Model):
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     thumb = models.ImageField(default='default.png', blank=True)
-    author = models.ForeignKey(User, on_delete=models.PROTECT, default=None)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,  on_delete=models.PROTECT, default=None)
 
     def __str__(self):
         return self.title
