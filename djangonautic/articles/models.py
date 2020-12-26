07ea4fb1a3bdeb327a2b772fdd6bf434674d8b1e
Harika <h.duyu@rebuy.com>
2020-12-26 14:02:06 +0100

    add author as foreign key

--- djangonautic/articles/models.py
+++ djangonautic/articles/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -7,8 +8,7 @@ class Article(models.Model):
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     thumb = models.ImageField(default='default.png', blank=True)
-    # image
-    # author
+    author = models.ForeignKey(User, on_delete=models.PROTECT, default=None)
 
     def __str__(self):
         return self.title
